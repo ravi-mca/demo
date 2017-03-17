e488061a62f971ab38c7d94171a16b6db7e6247f
sneha-zymr <snena.kinkar@zymr.com>
2017-03-17 17:08:19 +0530

    Create Header UI

--- ui/src/main.js
+++ ui/src/main.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Router, Route, Link, browserHistory, IndexRoute  } from 'react-router';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import Home from "./components/home/Home";
 import Merchands from "./components/merchands/Merchands";
 import Dataportals from "./components/Data-Portals/Dataportals";
