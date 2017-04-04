c5fc2437f58c40a97c66f1294212113e14b0712b
shwetarani-zymr <shwetarani.ghatge@zymr.com>
2017-04-04 12:22:11 +0530

    Updated name fields as per json response.

--- ui/src/components/merchants/Merchants.js
+++ ui/src/components/merchants/Merchants.js
@@ -45,10 +45,10 @@ export default class Merchants extends React.Component {
 		showAccountInfo = (
 			<div class="col-md-12 mt-10 mb-20 acc-border no-padding">
 				<div class="col-md-6 col-xs-6 auto-div no-padding">
-					<div class="acc-heading">{this.state.userInfo.firstname} </div>
+					<div class="acc-heading">{this.state.userInfo.firstName} </div>
 					<div class="acc-info">Account#: {this.state.userInfo.accountNo}</div>
 					<div class="acc-info">{this.state.userInfo.phone}</div>
-					<div class="acc-info mb-20">{this.state.userInfo.username}</div>
+					<div class="acc-info mb-20">{this.state.userInfo.email}</div>
 				</div>
 
 				<EditMerchants ref="editMerchantChild" data={this.state.userInfo} onUpdateAccount={this.getMerchantAccounts} />				
