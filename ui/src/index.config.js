fdbd70d31056be12ac2a172e0b9982e25d635231
shwetarani-zymr <shwetarani.ghatge@zymr.com>
2017-04-04 12:32:47 +0530

    Changed API path for edit merchant.

--- ui/src/index.config.js
+++ ui/src/index.config.js
@@ -2,7 +2,7 @@ var BaseUrl = 'http://localhost:8080';
 
 const Config = {
     createMerchant : BaseUrl +'/storecast/api/admin/merchant',
-    editMerchant : BaseUrl +'/storecast/api/admin/merchant',
+    editMerchant : BaseUrl +'/storecast/api/admin/merchant/',
     login : BaseUrl +'/storecast/api/admin/login',
     getMerchantList: BaseUrl + '/storecast/api/admin/merchant',
     getMerchant: BaseUrl + '/storecast/api/admin/merchant',
