acaa5247adcf51af13aa1ed6ce5bc0881281d89b
Sneha kinkar <sneha.k@ah.zymrinc.com>
2017-04-23 15:53:31 +0530

    Create responsive header

--- ui/src/components/header/Header.js
+++ ui/src/components/header/Header.js
@@ -1,27 +1,25 @@
 import React from 'react';
 import { Router, Route, Link, browserHistory, IndexRoute  } from 'react-router';
 import Background from '../../images/favendo-logo.png';
-import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'React-Bootstrap';
-import $ from 'jquery';
 
 export default class Header extends React.Component {
 	constructor(props) {
 	    super();
 
 	    
-	    //this.toggleChange = this.toggleChange.bind(this);
+	    this.toggleChange = this.toggleChange.bind(this);
 	}
 
     componentDidMount() {
-        $( "nav div" ).removeClass( "container" );
+       /* $( "nav div" ).removeClass( "container" );
         
 	    $(".navbar-toggle").html("<span></span>");
-        $(".navbar-toggle").html("<span class='fa fa-bars'></span>"); 
+        $(".navbar-toggle").html("<span class='fa fa-bars'></span>"); */
     }
 
-    /*toggleChange() {
+    toggleChange() {
 
-        if($(".navbar-collapse").hasClass( "collapse in" )) {
+        if($(".navbar-toggle").hasClass( "collapsed" )) {
        		$(".navbar-toggle").html("<span class='fa fa-bars'></span>");
        		$("li a").removeAttr("style"); 
         } else {
@@ -29,51 +27,21 @@ export default class Header extends React.Component {
         }
 
         $("li a:active").css("background-color", "#292e35"); 
-    }*/
-    render() {
-        return (
-        	<div>
-			 <Navbar inverse collapseOnSelect>
-    <Navbar.Header class="headerLogo col-lg-3">
-       <img src={ Background }/>
-      <Navbar.Toggle/>
-    </Navbar.Header>
-    <Navbar.Collapse >
-      <Nav class="nav navbar-nav ml-300">
-	     <NavItem eventKey={1} onClick={this.toggleChange}><Link to="/admins" activeClassName="active" class="text-white">ADMINS</Link></NavItem>
-	      <NavItem eventKey={2} onClick={this.toggleChange} ><Link to="/merchants" activeClassName="active" class="text-white">MERCHANTS</Link></NavItem>
-	      <NavItem eventKey={3} onClick={this.toggleChange}><Link to="/dataportal" activeClassName="active" class="text-white">DATA PORTAL</Link></NavItem>
-	   </Nav>
-      <Nav pullRight>
-       <NavItem eventKey={4} onClick={this.toggleChange}><Link to="/account" activeClassName="active" class="text-white">ACCOUNT</Link> </NavItem>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
-          	</div>
-        );
     }
-}
-
-
-/*
-
- <NavItem eventKey={1} onClick={this.toggleChange}><Link to="/admins" activeClassName="active" class="text-white">ADMINS</Link></NavItem>
-	      <NavItem eventKey={2} onClick={this.toggleChange}><Link to="/merchants" activeClassName="active" class="text-white">MERCHANTS</Link></NavItem>
-	      <NavItem eventKey={3} onClick={this.toggleChange}><Link to="/dataportal" activeClassName="active" class="text-white">DATA PORTAL</Link></NavItem>
-import React from 'react';
-import { Router, Route, Link, browserHistory, IndexRoute  } from 'react-router';
-import Background from '../../images/favendo-logo.png';
-
-export default class Header extends React.Component {
-
     render() {
         return (
         	<div>
 			<nav class="navbar navbar-inverse fixed-top">
 			  <div class="row">
-			    <div class="col-sm-4 col-md-4 col-lg-3 navbar-header">
+			    <div class="col-sm-12 col-sx-12 col-md-4 col-lg-3 navbar-header">
+			    <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle" onClick={this.toggleChange}>
+	                <span class="sr-only">Toggle navigation</span>
+	                <span class="fa fa-bars"></span>
+	            </button>
 			      <div class="headerLogo"> <img src={ Background }/></div>
 			    </div>
+
+			    <div id="navbarCollapse" class="collapse navbar-collapse">
 			    <div class="col-sm-offset-5 col-md-offset-5 col-lg-offset-5">
 			    <ul class="nav navbar-nav ">
 			      <li><Link to="/admins" activeClassName="active" class="text-white">ADMINS</Link></li>
@@ -82,17 +50,15 @@ export default class Header extends React.Component {
 			    </ul>
 			    </div>
 			    <div>
-			    <ul class="pull-nav">
+			    <ul class="nav navbar-nav pull-nav">
 			    	<li><Link to="/account" activeClassName="active" class="text-white">ACCOUNT</Link></li> 
 			    </ul>
 			    </div>
+			  </div>
 			  </div>
 			</nav>
           	</div>
         );
     }
-}*/
-
-
-
+}
 
