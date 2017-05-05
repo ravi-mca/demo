57d792b55f9d1e9b6fc3d54a74dabfb77937444a
Krushal Gajjar <Krushal.gajjar@zymr.com>
2017-05-05 11:45:03 +0530

    Refctor changes

--- server/store-service/src/main/java/com/favendo/store/service/dao/StoreDao.java
+++ server/store-service/src/main/java/com/favendo/store/service/dao/StoreDao.java
@@ -16,16 +16,20 @@ public interface StoreDao extends JpaRepository<Store, Long> {
             "from Store store " +
             "where upper(store.name) = upper(:name) OR " +
             "upper(store.nickName) = upper(:nickName) ")
-    Store findByNameOrNickName(@Param("name") String name, @Param("nickName") String nickName);
+    Store findByNameOrNickName(@Param("name") String name,
+                               @Param("nickName") String nickName);
 
     @Query("select store " +
             "from Store store " +
             "where (upper(store.name) = upper(:name) OR " +
             "upper(store.nickName) = upper(:nickName)) AND " +
             "store.id <> :id")
-    Store findByNameOrNickNameAndId(@Param("name") String username, @Param("nickName")
-            String accountName, @Param("id") Long id);
+    Store findByNameOrNickNameAndId(@Param("name") String username,
+                                    @Param("nickName") String accountName,
+                                    @Param("id") Long id);
     
-    @Query("select store from Store as store join store.merchant as merchant where merchant.merchantId = :merchantId ORDER BY store.name ASC")
+    @Query("select store " +
+            "from Store as store join store.merchant as merchant " +
+            "where merchant.merchantId = :merchantId ORDER BY store.name ASC")
     List<Store> findAllByMerchantId(@Param("merchantId") Long merchantId);
 }
